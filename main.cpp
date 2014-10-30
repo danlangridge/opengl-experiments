37eef3397d6d7ebaf52597d87fe3997c5ff2916d
Dan <dlangridge@gmail.com>
2014-10-29 23:31:04 -0700

    Removed windows library

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@
 // Include GLEW
 #include <GL/glew.h>
 
-// Include GLFW
-#include <glfw3.h>
-GLFWwindow* window;
-
 // Include GLM
 #include <glm/glm.hpp>
 using namespace glm;
@@ -17,26 +13,7 @@ using namespace glm;
 
 int main( void )
 {
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		return -1;
-	}
-
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
+	//TODO: Initialize GLEW
 
 	// Initialize GLEW
 	glewExperimental = true; // Needed for core profile
@@ -45,8 +22,6 @@ int main( void )
 		return -1;
 	}
 
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
@@ -95,21 +70,13 @@ int main( void )
 
 		glDisableVertexAttribArray(0);
 
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
 
 	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
-
+	
 	// Cleanup VBO
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteVertexArrays(1, &VertexArrayID);
 	glDeleteProgram(programID);
-
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
 
 	return 0;
 }
